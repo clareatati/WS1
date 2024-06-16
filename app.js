50fb1b3acdc3a1f5cb640bb8fb46f451cbdd796a
clareatati <ataticlare@gmail.com>
2024-06-16 22:33:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function openTab(tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.classList.add("active"); 
+  }
+  
+  // Show InSAR tab by default (you can change this if needed)
+  document.getElementById("InSAR").style.display = "block"; 
+  
